b56b76dbf77c3cfc574af4940afd211099f288bd
FedeMR21 <72584280+FedeMR21@users.noreply.github.com>
2021-04-03 22:36:29 -0300

    Commit Inicial Eclipse(STS4) Close #1

---
+++ Portafolios/src/main/java/ar/com/federicomorenorodriguez/sitio/PortafoliosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.federicomorenorodriguez.sitio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafoliosApplication.class, args);
+	}
+
+}
